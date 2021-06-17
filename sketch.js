5cf0a8488aa332859cc699de1ae313513e3367b3
Henry Pehr <48739094+henrypehr@users.noreply.github.com>
2021-06-17 10:46:37 -0400

    added comments

--- sketch.js
+++ sketch.js
@@ -1,22 +1,26 @@
+// I might have too many global variables
 let walls = [];
 let res = 20;
 let field = [];
 let x, y;
 let particle;
 let time = 0;
+// the low-pass filter value for creating walls from the noise
 let limit = 0.6;
 let cols = 600/res;
 let rows = 600/res;
+// the scaling factor of the noise
 let detail = 0.1;
+// these two affect how the particle moves in the environment
 let particle_rxn = 0.01;
 let particle_speed = 20;
+// show rays as lines, maybe make this a toggle button in browser?
 let show_rays = false;
 
 
 
 function setup() {
   createCanvas(600, 600);
-  // make walls around the edges
 }
 
 function pass(x) {
@@ -29,7 +33,7 @@ function pass(x) {
 function getState(a, b, c, d) {
   return a * 8 + b * 4 + c * 2 + d * 1;
 }
-
+// little function that moves particle to where you click. was for debugging, now a feature!
 function mouseClicked() {
   x = mouseX;
   y = mouseY;
@@ -38,7 +42,9 @@ function mouseClicked() {
 
 function draw() {
   background(0);
+  // pass time as a parameter to wallgen because we need it for the perlin noise
   wallgenerator = new wallgen(walls, time);
+  // make walls around the edges
   walls.push(new Boundary(createVector(0, 0), createVector(width, 0)));
   walls.push(new Boundary(createVector(width, 0), createVector(width, height)));
   walls.push(new Boundary(createVector(width, height), createVector(0, height)));
@@ -47,21 +53,15 @@ function draw() {
     wall.show();
   }
   stroke(225);
-  // for (let x=0;x<30;x=x+0.5) {
-  //   for (var y=0;y<30;y=y+0.5) {
-  //       if (noise(x/detail,y/detail+time,time)>limit) {
-  //         point(x*res,y*res);
-  //         strokeWeight(noise(x/detail,y/detail+time,time)*10);
-  //       }
-  //   }
-  // }
   strokeWeight(1);
   if (particle) {
     move = createVector(0,0);
+    // three big particle functions that make most things happen
     particle.look(walls);
     particle.movement();
     particle.show();
   }
+  // increment time for noise
   time=time+0.001;
   walls = [];
 }
